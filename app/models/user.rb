d7ba0eda32822175dbf8611d5794ecd850746d53
Coders Manic <manicstreetcoders@gmail.com>
2012-06-27 17:48:36 +0900

    Remove twitter things from the view.

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,12 +31,4 @@ class User < ActiveRecord::Base
     end
     user
   end
-
-  def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
-      end
-    end
-  end
 end
