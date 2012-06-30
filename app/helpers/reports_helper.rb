cee7c6e25270e582b967e9125ff9c98ef1f06533
Coders Manic <manicstreetcoders@gmail.com>
2012-06-30 21:55:52 +0900

    Display a email of uploader in search result

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -6,4 +6,13 @@ module ReportsHelper
     end
     "#{i}d ago"
   end
+
+  def email( user_id )
+    user = User.find_by_id(user_id)
+    if user
+      return user.email
+    else
+      return ""
+    end
+  end
 end
