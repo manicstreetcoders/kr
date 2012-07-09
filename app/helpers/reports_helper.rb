31d0973015f38c217dafbf3ba54e0ccb870422bf
Coders Manic <manicstreetcoders@gmail.com>
2012-07-09 17:08:11 +0900

    Add avatar image to reports#show

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,6 +1,15 @@
 module ReportsHelper
   include ActsAsTaggableOn::TagsHelper
 
+  def avatar_url( user_id)
+    user = User.find_by_id(user_id)
+    if user
+      return user.avatar.url(:thumb)
+    else
+      return ""
+    end
+  end
+
   def email( user_id )
     user = User.find_by_id(user_id)
     if user
