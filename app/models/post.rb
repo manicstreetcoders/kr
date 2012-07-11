9ec4b3dd07c710facbd5c005817e126da8b83368
Coders Manic <manicstreetcoders@gmail.com>
2012-07-11 18:15:08 +0900

    Add a board

---
+++ app/models/post.rb
@@ -0,0 +1,8 @@
+class Post < ActiveRecord::Base
+  attr_accessible :content, :title, :user_id
+
+  validates :title, :presence => true, :length => { :minimum => 3 }
+  validates :content, :presence => true, :length => { :minimum => 3 }
+
+  belongs_to :user
+end
