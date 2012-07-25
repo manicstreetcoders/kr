54e775cdc28d925e82e320703eebac5ef98934d9
Coders Manic <manicstreetcoders@gmail.com>
2012-07-25 18:18:39 +0900

    Display reply count @ posts#index

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,14 @@
 module PostsHelper
+  def post_title_with_reply_count( post )
+    if post && post.title
+      comments = post.comment_threads
+      count = comments.length
+      if count > 0
+        return post.title + " [" + count.to_s() + "]"
+      else
+        return post.title
+      end
+    end
+    ""
+  end
 end
