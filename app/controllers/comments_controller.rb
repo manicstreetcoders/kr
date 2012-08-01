9f7d2b02a1cd89f58ee392dc703cb556800e316a
Coders Manic <manicstreetcoders@gmail.com>
2012-08-01 13:22:11 +0900

    + deleting comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,6 +9,7 @@ class CommentsController < ApplicationController
   def destroy
     @post = Post.find(params[:post_id])
     @comment = Comment.find(params[:id])
+    @comment.destroy
     redirect_to post_path(@post)
   end
 end
