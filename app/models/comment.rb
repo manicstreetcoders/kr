0a04836efc625d4ba532b04748a33f059dae7980
Coders Manic <manicstreetcoders@gmail.com>
2012-08-01 14:11:20 +0900

    Change the listing order of comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -13,6 +13,8 @@ class Comment < ActiveRecord::Base
   # NOTE: Comments belong to a user
   belongs_to :user
 
+  default_scope :order => 'comments.id ASC'
+
   # Helper class method that allows you to build a comment
   # by passing a commentable object, a user_id, and comment text
   # example in readme
